64ad3f8dfa0f2b517e68670f4e712d545fc596dc
mostak9 <mostak.ahm263@gmail.com>
2024-01-06 02:17:25 +0600

    addes aos animation to the stat section

--- client-side/src/Components/Home/SiteStat/SiteStat.jsx
+++ client-side/src/Components/Home/SiteStat/SiteStat.jsx
@@ -1,9 +1,14 @@
 import { Container, Grid, Typography } from "@mui/material";
-import CountUp from 'react-countup';
+import CountUp from "react-countup";
 
 const SiteStat = () => {
   return (
-    <Grid bgcolor="rgba(220, 20, 60, 1)" py={8}>
+    <Grid
+      data-aos="fade-up"
+      data-aos-duration="1500"
+      bgcolor="rgba(220, 20, 60, 1)"
+      py={8}
+    >
       <Container>
         <Grid sx={{ color: "white", textAlign: "center" }}>
           <Typography variant="h4" fontWeight={700}>
@@ -18,63 +23,85 @@ const SiteStat = () => {
             item
             p={3}
             borderRight={{ xs: "none", md: "2px solid white" }}
-            xs={12} md={6} lg={3}
-            display={'flex'}
-            justifyContent={'center'}
-            alignItems={'center'}
-            flexDirection={'column'}
-            color={'white'}
+            xs={12}
+            md={6}
+            lg={3}
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            flexDirection={"column"}
+            color={"white"}
             gap={2}
           >
-            <Typography variant="h5" fontWeight={700}><CountUp duration={5} end={50000}/>+ / year</Typography>
-            <Typography variant="body"  fontWeight={500}>Successful Donation</Typography>
+            <Typography variant="h5" fontWeight={700}>
+              <CountUp duration={5} end={50000} />+ / year
+            </Typography>
+            <Typography variant="body" fontWeight={500}>
+              Successful Donation
+            </Typography>
           </Grid>
           <Grid
             item
             p={3}
             borderRight={{ xs: "none", md: "2px solid white" }}
-            xs={12} md={6} lg={3}
-            display={'flex'}
-            justifyContent={'center'}
-            alignItems={'center'}
-            flexDirection={'column'}
-            color={'white'}
+            xs={12}
+            md={6}
+            lg={3}
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            flexDirection={"column"}
+            color={"white"}
             gap={2}
           >
-            <Typography variant="h5" fontWeight={700}><CountUp duration={5} end={1000}/>+ / month</Typography>
-            <Typography variant="body"  fontWeight={500}>Active Donor</Typography>
+            <Typography variant="h5" fontWeight={700}>
+              <CountUp duration={5} end={1000} />+ / month
+            </Typography>
+            <Typography variant="body" fontWeight={500}>
+              Active Donor
+            </Typography>
           </Grid>
           <Grid
             item
             p={3}
             borderRight={{ xs: "none", md: "2px solid white" }}
-            xs={12} md={6} lg={3}
-            display={'flex'}
-            justifyContent={'center'}
-            alignItems={'center'}
-            flexDirection={'column'}
-            color={'white'}
+            xs={12}
+            md={6}
+            lg={3}
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            flexDirection={"column"}
+            color={"white"}
             gap={2}
           >
-            <Typography variant="h5" fontWeight={700}><CountUp duration={5} end={30}/>+</Typography>
-            <Typography variant="body"  fontWeight={500}>Affiliated Hospitals</Typography>
+            <Typography variant="h5" fontWeight={700}>
+              <CountUp duration={5} end={30} />+
+            </Typography>
+            <Typography variant="body" fontWeight={500}>
+              Affiliated Hospitals
+            </Typography>
           </Grid>
           <Grid
             item
             p={3}
-            
-            xs={12} md={6} lg={3}
-            display={'flex'}
-            justifyContent={'center'}
-            alignItems={'center'}
-            flexDirection={'column'}
-            color={'white'}
+            xs={12}
+            md={6}
+            lg={3}
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            flexDirection={"column"}
+            color={"white"}
             gap={2}
           >
-            <Typography variant="h5" fontWeight={700}><CountUp duration={5} end={70}/>%</Typography>
-            <Typography variant="body"  fontWeight={500}>Emergency Response</Typography>
+            <Typography variant="h5" fontWeight={700}>
+              <CountUp duration={5} end={70} />%
+            </Typography>
+            <Typography variant="body" fontWeight={500}>
+              Emergency Response
+            </Typography>
           </Grid>
-         
         </Grid>
       </Container>
     </Grid>
